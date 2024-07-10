9433d086bb64ea85d83201240db6d0ca5393e055
RajbharSaheb <110810360+RajbharSaheb@users.noreply.github.com>
2024-07-10 08:16:15 +0530

    Update channel.py

--- plugins/channel.py
+++ plugins/channel.py
@@ -1,6 +1,6 @@
 from pyrogram import Client, filters
 from info import CHANNELS
-from database.aks_files import save_file
+from database.ia_filterdb.py import save_file
 
 media_filter = filters.document | filters.video
 
